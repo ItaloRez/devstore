270379314ebf7c67a7cd7a55310698bc48befadf
Ítalo Rezende <italo.rezende@sharkdev.com.br>
2023-10-28 22:43:50 -0300

    feat: add first discount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+
+export default function Home() {
+  const { data } = useSession();
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        alt="Até 55% de desconto esse mês!"
+        width={0}
+        height={0}
+        className="h-auto w-full"
+        sizes="100vw"
+      />
+    </div>
+  );
+}
